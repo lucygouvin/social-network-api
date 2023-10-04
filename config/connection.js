6e0781afdfce7bc212798b9068470fa471239cac
lgouvinSchell <gouvin.lucy@gmail.com>
2023-10-03 21:35:17 -0400

    update heroku db connection

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/videosAndResponses');
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB';
+
+connect(connectionString);
 
 module.exports = connection;
