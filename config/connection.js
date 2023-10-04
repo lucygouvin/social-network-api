a3ca1752f817c7f550cfcbe476208f17c462cefd
lgouvinSchell <gouvin.lucy@gmail.com>
2023-10-04 18:16:01 -0400

    db debug

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB';
+  process.env.MONGODB_URI || 'mongodb+srv://lgouvin:sAXfVn6dJY2WGxKO@cluster0.poferpf.mongodb.net/social-network?retryWrites=true&w=majority';
 // force rebuild
 connect(connectionString);
 
